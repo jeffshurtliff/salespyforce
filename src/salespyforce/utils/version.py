97a5b451254135b65271648e6fc9899fdc5a234b
Jeff Shurtliff <jeff.shurtliff@rsa.com>
2023-01-22 14:54:37 -0700

    Created the version.py file

---
+++ src/salespyforce/utils/version.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+:Module:            salespyforce.utils.version
+:Synopsis:          This simple script contains the package version
+:Created By:        Jeff Shurtliff
+:Last Modified:     Jeff Shurtliff
+:Modified Date:     22 Jan 2023
+"""
+
+from . import log_utils
+
+# Initialize logging
+logger = log_utils.initialize_logging(__name__)
+
+# Define special and global variables
+__version__ = "1.0.0"
+
+
+def get_full_version():
+    """This function returns the current full version of the ``salespyforce`` package."""
+    return __version__
+
+
+def get_major_minor_version():
+    """This function returns the current major.minor (i.e. X.Y) version of the ``salespyforce`` package."""
+    return ".".join(__version__.split(".")[:2])
