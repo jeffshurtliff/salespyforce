63a62a198bb0ba5a913460211f08eb329ccd3c69
Jeff Shurtliff <jeff.shurtliff@rsa.com>
2023-01-22 16:33:16 -0700

    Added basic API calls to the core object

--- src/salespyforce/core.py
+++ src/salespyforce/core.py
@@ -79,9 +79,22 @@ class Salesforce(object):
             _connection_info[_field] = ''
         return _connection_info
 
+    def _get_headers(self, header_type='default'):
+        """This method returns the appropriate HTTP headers to use for different types of API calls."""
+        headers = {
+            'content-type': 'application/json',
+            'accept-encoding': 'gzip',
+            'authorization': f'Bearer {self.access_token}'
+        }
+        if header_type == 'articles':
+            headers['accept-language'] = 'en-US'
+        return headers
+
     def connect(self):
         """This method connects to the Salesforce instance to obtain the access token.
         Reference: https://jereze.com/code/authentification-salesforce-rest-api-python/
+
+        :returns: The API call response with the authorization information
         """
         params = {
             'grant_type': 'password',
@@ -94,6 +107,170 @@ class Salesforce(object):
         if response.status_code != 200:
             raise RuntimeError(f'Failed to connect to the Salesforce instance.\n{response.text}')
         return response.json()
+
+    def get(self, endpoint, params=None, headers=None, timeout=30, show_full_error=True, return_json=True):
+        """This method performs a GET request against the Salesforce instance.
+        Reference: https://jereze.com/code/authentification-salesforce-rest-api-python/
+
+        :param endpoint: The API endpoint to query
+        :type endpoint: str
+        :param params: The query parameters (where applicable)
+        :type params: dict, None
+        :param headers: Specific API headers to use when performing the API call
+        :type headers: dict, None
+        :param timeout: The timeout period in seconds (defaults to ``30``)
+        :type timeout: int, str, None
+        :param show_full_error: Determines if the full error message should be displayed (defaults to ``True``)
+        :type show_full_error: bool
+        :param return_json: Determines if the response should be returned in JSON format (defaults to ``True``)
+        :returns: The API response in JSON format or as a ``requests`` object
+        """
+        # Define the parameters as an empty dictionary if none are provided
+        params = {} if params is None else params
+
+        # Define the headers
+        default_headers = self._get_headers()
+        headers = default_headers if not headers else headers
+
+        # Make sure the endpoint begins with a slash
+        endpoint = f'/{endpoint}' if not endpoint.startswith('/') else endpoint
+
+        # Perform the API call
+        response = requests.get(f'{self.instance_url}{endpoint}', headers=headers, params=params, timeout=timeout)
+        if response.status_code >= 300:
+            if show_full_error:
+                raise RuntimeError(f'The GET request failed with a {response.status_code} status code.\n'
+                                   f'{response.text}')
+            else:
+                raise RuntimeError(f'The GET request failed with a {response.status_code} status code.')
+        if return_json:
+            response = response.json()
+        return response
+
+    def api_call_with_payload(self, method, endpoint, payload, params=None, headers=None, timeout=30,
+                              show_full_error=True, return_json=True):
+        """This method performs a POST call against the Salesforce instance.
+        Reference: https://jereze.com/code/authentification-salesforce-rest-api-python/
+
+        :param method: The API method (``post``, ``put``, or ``patch``)
+        :type method: str
+        :param endpoint: The API endpoint to query
+        :type endpoint: str
+        :param payload: The payload to leverage in the API call
+        :type payload: dict
+        :param params: The query parameters (where applicable)
+        :type params: dict, None
+        :param headers: Specific API headers to use when performing the API call
+        :type headers: dict, None
+        :param timeout: The timeout period in seconds (defaults to ``30``)
+        :type timeout: int, str, None
+        :param show_full_error: Determines if the full error message should be displayed (defaults to ``True``)
+        :type show_full_error: bool
+        :param return_json: Determines if the response should be returned in JSON format (defaults to ``True``)
+        :returns: The API response in JSON format or as a ``requests`` object
+        """
+        # Define the parameters as an empty dictionary if none are provided
+        params = {} if params is None else params
+
+        # Define the headers
+        default_headers = self._get_headers()
+        headers = default_headers if not headers else headers
+
+        # Make sure the endpoint begins with a slash
+        endpoint = f'/{endpoint}' if not endpoint.startswith('/') else endpoint
+
+        # Perform the API call
+        if method.lower() == 'post':
+            response = requests.post(f'{self.instance_url}{endpoint}', json=payload, headers=headers, params=params,
+                                     timeout=timeout)
+        elif method.lower() == 'patch':
+            response = requests.patch(f'{self.instance_url}{endpoint}', json=payload, headers=headers, params=params,
+                                      timeout=timeout)
+        elif method.lower() == 'put':
+            response = requests.put(f'{self.instance_url}{endpoint}', json=payload, headers=headers, params=params,
+                                    timeout=timeout)
+        else:
+            raise ValueError('The API call method (POST or PATCH OR PUT) must be defined.')
+
+        # Examine the result
+        if response.status_code >= 300:
+            if show_full_error:
+                raise RuntimeError(f'The POST request failed with a {response.status_code} status code.\n'
+                                   f'{response.text}')
+            else:
+                raise RuntimeError(f'The POST request failed with a {response.status_code} status code.')
+        if return_json:
+            try:
+                response = response.json()
+            except Exception as exc:
+                print(f'Failed to convert the API response to JSON format due to the following exception: {exc}')
+        return response
+
+    def post(self, endpoint, payload, params=None, headers=None, timeout=30, show_full_error=True, return_json=True):
+        """This method performs a POST call against the Salesforce instance.
+        Reference: https://jereze.com/code/authentification-salesforce-rest-api-python/
+
+        :param endpoint: The API endpoint to query
+        :type endpoint: str
+        :param payload: The payload to leverage in the API call
+        :type payload: dict
+        :param params: The query parameters (where applicable)
+        :type params: dict, None
+        :param headers: Specific API headers to use when performing the API call
+        :type headers: dict, None
+        :param timeout: The timeout period in seconds (defaults to ``30``)
+        :type timeout: int, str, None
+        :param show_full_error: Determines if the full error message should be displayed (defaults to ``True``)
+        :type show_full_error: bool
+        :param return_json: Determines if the response should be returned in JSON format (defaults to ``True``)
+        :returns: The API response in JSON format or as a ``requests`` object
+        """
+        return self.api_call_with_payload('post', endpoint, payload, params=params, headers=headers, timeout=timeout,
+                                          show_full_error=show_full_error, return_json=return_json)
+
+    def patch(self, endpoint, payload, params=None, headers=None, timeout=30, show_full_error=True, return_json=False):
+        """This method performs a PATCH call against the Salesforce instance.
+        Reference: https://jereze.com/code/authentification-salesforce-rest-api-python/
+
+        :param endpoint: The API endpoint to query
+        :type endpoint: str
+        :param payload: The payload to leverage in the API call
+        :type payload: dict
+        :param params: The query parameters (where applicable)
+        :type params: dict, None
+        :param headers: Specific API headers to use when performing the API call
+        :type headers: dict, None
+        :param timeout: The timeout period in seconds (defaults to ``30``)
+        :type timeout: int, str, None
+        :param show_full_error: Determines if the full error message should be displayed (defaults to ``True``)
+        :type show_full_error: bool
+        :param return_json: Determines if the response should be returned in JSON format (defaults to ``True``)
+        :returns: The API response in JSON format or as a ``requests`` object
+        """
+        return self.api_call_with_payload('patch', endpoint, payload, params=params, headers=headers, timeout=timeout,
+                                          show_full_error=show_full_error, return_json=return_json)
+
+    def put(self, endpoint, payload, params=None, headers=None, timeout=30, show_full_error=True, return_json=True):
+        """This method performs a PUT call against the Salesforce instance.
+        Reference: https://jereze.com/code/authentification-salesforce-rest-api-python/
+
+        :param endpoint: The API endpoint to query
+        :type endpoint: str
+        :param payload: The payload to leverage in the API call
+        :type payload: dict
+        :param params: The query parameters (where applicable)
+        :type params: dict, None
+        :param headers: Specific API headers to use when performing the API call
+        :type headers: dict, None
+        :param timeout: The timeout period in seconds (defaults to ``30``)
+        :type timeout: int, str, None
+        :param show_full_error: Determines if the full error message should be displayed (defaults to ``True``)
+        :type show_full_error: bool
+        :param return_json: Determines if the response should be returned in JSON format (defaults to ``True``)
+        :returns: The API response in JSON format or as a ``requests`` object
+        """
+        return self.api_call_with_payload('put', endpoint, payload, params=params, headers=headers, timeout=timeout,
+                                          show_full_error=show_full_error, return_json=return_json)
 
 
 def define_connection_info():
