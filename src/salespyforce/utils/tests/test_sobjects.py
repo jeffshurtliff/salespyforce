21d7bf545192e6e414c2ec0637975108b719e060
Jeff Shurtliff <jeff.shurtliff@rsa.com>
2023-05-29 12:03:46 -0600

    Added test_sobjects.py and test_soql.py with defined pytest functions

---
+++ src/salespyforce/utils/tests/test_sobjects.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+:Module:         khoros.utils.tests.test_sobjects
+:Synopsis:       This module is used by pytest to test basic sObject-related methods
+:Created By:     Jeff Shurtliff
+:Last Modified:  Jeff Shurtliff
+:Modified Date:  29 May 2023
+"""
+
+from . import resources
+
+
+def test_get_all_sobjects():
+    """This function tests the get_all_sobjects() method in the core object.
+
+    .. versionadded:: 1.1.0
+    """
+    sfdc_object = resources.get_core_object()
+    all_sobjects = sfdc_object.get_all_sobjects()
+    assert 'sobjects' in all_sobjects
+
+
+def test_get_and_describe_sobject():
+    """This function tests the get_sobject() and describe_object() methods in the core object.
+
+    .. versionadded:: 1.1.0
+    """
+    # Instantiate the core object
+    sfdc_object = resources.get_core_object()
+
+    # Test the default query (non-describe)
+    account_sobject = sfdc_object.get_sobject('Account')
+    assert 'objectDescribe' in account_sobject
+
+    # Test with describe enabled
+    account_sobject_describe = sfdc_object.get_sobject('Account', describe=True)
+    assert 'activateable' in account_sobject_describe
+
+    # Test the describe_object() method
+    account_describe = sfdc_object.describe_object('Account')
+    assert 'activateable' in account_describe
+
---
+++ src/salespyforce/utils/tests/test_soql.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+:Module:         khoros.utils.tests.test_sobjects
+:Synopsis:       This module is used by pytest to test performing SOQL queries
+:Created By:     Jeff Shurtliff
+:Last Modified:  Jeff Shurtliff
+:Modified Date:  29 May 2023
+"""
+
+from . import resources
+
+
+def test_soql_query():
+    """This function tests the ability to perform a SOQL query.
+
+    .. versionadded:: 1.1.0
+    """
+    sfdc_object = resources.get_core_object()
+    soql_statement = 'SELECT Id FROM Account LIMIT 1'
+    soql_response = sfdc_object.soql_query(soql_statement)
+    assert 'done' in soql_response and soql_response.get('done') is True
+    assert 'totalSize' in soql_response and 'records' in soql_response
+
