2f87e578bcab296636f64db8127d10dfb09ce8f4
Jeff Shurtliff <jeff.shurtliff@rsa.com>
2026-02-03 09:51:00 -0700

    Added some type hints and other similar changes to chatter.py

--- src/salespyforce/chatter.py
+++ src/salespyforce/chatter.py
@@ -4,27 +4,32 @@
 :Synopsis:          Defines the Chatter-related functions associated with the Salesforce Connect API
 :Created By:        Jeff Shurtliff
 :Last Modified:     Jeff Shurtliff
-:Modified Date:     13 Mar 2023
+:Modified Date:     03 Feb 2026
 """
 
+from __future__ import annotations
+
+from typing import Optional
+
+from . import errors
 from .utils import log_utils
 
 # Initialize logging
 logger = log_utils.initialize_logging(__name__)
 
 
-def _get_site_endpoint_segment(_site_id=None):
+def _get_site_endpoint_segment(_site_id: Optional[str] = None) -> str:
     """This function constructs the endpoint segment when querying a specific Experience Cloud site.
 
     :param _site_id: The Site ID of the Experience Cloud site
-    :type _site_id: str
+    :type _site_id: str, None
     :returns: The API endpoint segment (or a blank string if no Site ID was provided)
     """
     _endpoint_segment = f'/connect/communities/{_site_id}' if _site_id else ''
     return _endpoint_segment
 
 
-def get_my_news_feed(sfdc_object, site_id=None):
+def get_my_news_feed(sfdc_object, site_id: Optional[str] = None):
     """This function retrieves the news feed for the user calling the function.
     (`Reference <https://developer.salesforce.com/docs/atlas.en-us.chatterapi.meta/chatterapi/quickreference_get_news_feed.htm>`_)
 
@@ -40,7 +45,7 @@ def get_my_news_feed(sfdc_object, site_id=None):
     return sfdc_object.get(endpoint)
 
 
-def get_user_news_feed(sfdc_object, user_id, site_id=None):
+def get_user_news_feed(sfdc_object, user_id: str, site_id: Optional[str] = None):
     """This function retrieves another user's news feed.
     (`Reference <https://developer.salesforce.com/docs/atlas.en-us.chatterapi.meta/chatterapi/quickreference_get_user_profile_feed.htm>`_)
 
@@ -58,7 +63,7 @@ def get_user_news_feed(sfdc_object, user_id, site_id=None):
     return sfdc_object.get(endpoint)
 
 
-def get_group_feed(sfdc_object, group_id, site_id=None):
+def get_group_feed(sfdc_object, group_id: str, site_id: Optional[str] = None):
     """This function retrieves a group's news feed.
     (`Reference <https://developer.salesforce.com/docs/atlas.en-us.chatterapi.meta/chatterapi/quickreference_get_group_feed.htm>`_)
 
@@ -76,9 +81,19 @@ def get_group_feed(sfdc_object, group_id, site_id=None):
     return sfdc_object.get(endpoint)
 
 
-def post_feed_item(sfdc_object, subject_id, message_text=None, message_segments=None, site_id=None, created_by_id=None):
+def post_feed_item(
+        sfdc_object,
+        subject_id: str,
+        message_text: Optional[str] = None,
+        message_segments: Optional[list] = None,
+        site_id: Optional[str] = None,
+        created_by_id: Optional[str] = None):
     """This function publishes a new Chatter feed item.
     (`Reference <https://developer.salesforce.com/docs/atlas.en-us.chatterapi.meta/chatterapi/quickreference_post_feed_item.htm>`_)
+
+    .. version-changed:: 1.4.0
+       The function now raises the :py:exc:`salespyforce.errors.exceptions.MissingRequiredDataError` exception rather than the
+       generic :py:exc:`RuntimeError` exception.
 
     :param sfdc_object: The instantiated SalesPyForce object
     :type sfdc_object: class[salespyforce.Salesforce]
@@ -93,11 +108,12 @@ def post_feed_item(sfdc_object, subject_id, message_text=None, message_segments=
     :param created_by_id: The ID of the user to impersonate (**Experimental**)
     :type created_by_id: str, None
     :returns: The response of the POST request
-    :raises: :py:exc:`RuntimeError`
+    :raises: :py:exc:`RuntimeError`,
+             :py:exc:`salespyforce.errors.exceptions.MissingRequiredDataError`
     """
     site_segment = _get_site_endpoint_segment(site_id)
     if not any((message_text, message_segments)):
-        raise RuntimeError('Message text or message segments are required to post a feed item.')
+        raise errors.exceptions.MissingRequiredDataError('Message text or message segments are required to post a feed item.')
     if not message_segments:
         message_segments = _construct_simple_message_segment(message_text)
     body = {
@@ -114,7 +130,13 @@ def post_feed_item(sfdc_object, subject_id, message_text=None, message_segments=
     return sfdc_object.post(endpoint=endpoint, payload=body)
 
 
-def post_comment(sfdc_object, feed_element_id, message_text=None, message_segments=None, site_id=None, created_by_id=None):
+def post_comment(
+        sfdc_object,
+        feed_element_id: str,
+        message_text: Optional[str] = None,
+        message_segments: Optional[list] = None,
+        site_id: Optional[str] = None,
+        created_by_id: Optional[str] = None):
     """This function publishes a comment on a Chatter feed item.
     (`Reference <https://developer.salesforce.com/docs/atlas.en-us.chatterapi.meta/chatterapi/quickreference_post_comment_to_feed_element.htm>`_)
 
@@ -123,7 +145,7 @@ def post_comment(sfdc_object, feed_element_id, message_text=None, message_segmen
     :param feed_element_id: The ID of the feed element on which to post the comment
     :type feed_element_id: str
     :param message_text: Plaintext to be used as the message body
-    :type message_segments: str, None
+    :type message_text: str, None
     :param message_segments: Collection of message segments to use instead of a plaintext message
     :type message_segments: list, None
     :param site_id: The ID of an Experience Cloud site against which to query (optional)
@@ -136,6 +158,7 @@ def post_comment(sfdc_object, feed_element_id, message_text=None, message_segmen
     site_segment = _get_site_endpoint_segment(site_id)
     if not any((message_text, message_segments)):
         raise RuntimeError('Message text or message segments are required to post a feed comment.')
+
     if not message_segments:
         message_segments = _construct_simple_message_segment(message_text)
     body = {
@@ -144,6 +167,7 @@ def post_comment(sfdc_object, feed_element_id, message_text=None, message_segmen
         }
     }
     if created_by_id:
+        # noinspection PyTypeChecker
         body['createdById'] = created_by_id
     endpoint = f'/services/data/{sfdc_object.version}{site_segment}/chatter/feed-elements/' \
                f'{feed_element_id}/capabilities/comments/items'
